526d61e2545232275a133d6dddb8e5fd49f0f694
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-25 11:44:32 -0300

    use routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './global.css'
 
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
 
   return (
     <div>
-      <Logon />
+      <Routes />
     </div>
   );
 }
