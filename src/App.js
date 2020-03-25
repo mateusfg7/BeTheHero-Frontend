b4c6d836bffd72b7d7b3b46c47c442ec4803434a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-25 10:17:02 -0300

    add logon files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
       <h1>Hello Word!</h1>
     </div>
   );
